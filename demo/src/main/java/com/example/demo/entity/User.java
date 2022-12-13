834579cdcbda3aab1eabef55be3ade748fa0e8f2
sungchulyun <proattacker@naver.com>
2022-12-13 19:01:05 +0900

    init

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -3,7 +3,10 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -23,7 +26,12 @@ public class User {
     private EncryptionAlgorithm algorithm;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
-    private List<Authority> authorities;
+    private ArrayList<GrantedAuthority> authorities;
+
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        ArrayList<GrantedAuthority> authList = new ArrayList<GrantedAuthority>(authorities);
+        return authList;
+    }
 
     public enum EncryptionAlgorithm {
         BCRYPT
--- demo/src/main/java/com/example/demo/security/ProjectConfig.java
+++ demo/src/main/java/com/example/demo/security/ProjectConfig.java
@@ -1,13 +1,21 @@
 package com.example.demo.security;
 
+import com.example.demo.service.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,27 +24,42 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class ProjectConfig{
 
-    @Autowired
-    private AuthenticationProviderService authenticationProvider;
+   @Bean
+   public UserDetailsService userDetailsService(){
+       var manager = new InMemoryUserDetailsManager();
+
+       var user1 = User.withUsername("john")
+               .password("12345")
+               .authorities("READ")
+               .build();
+
+       var user2 = User.withUsername("jane")
+               .password("12345")
+               .authorities("WRITE")
+               .build();
+
+       manager.createUser(user1);
+       manager.createUser(user2);
+       return manager;
+   }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 
         http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .hasAnyAuthority("WRITE","READ")
+                .and()
                 .formLogin()
-                .defaultSuccessUrl("/main", true);
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-
-                .httpBasic(withDefaults());
+                .defaultSuccessUrl("/hello");
         return http.build();
     }
-
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
